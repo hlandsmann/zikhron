8e2d3b840a4658202dd2a377255262030127e0bb
Harmen Landsmann <you@example.com>
2020-09-12 16:57:41 +0200

    implement markup word

--- src/gui/HtmlGen.cpp
+++ src/gui/HtmlGen.cpp
@@ -1,1 +1,47 @@
 #include "HtmlGen.h"
+#include <fmt/format.h>
+
+namespace markup {
+
+auto Word::joinCharactersNonBreakable(const utl::StringU8& word) const -> std::string {
+    std::string result;
+    word.iterate_characters(
+        [&result](const std::string_view& sv) { result += std::string(sv) + "&#8288;"; }, 0, -1);
+    result += word.back();
+
+    return result;
+}
+
+auto Word::lengthOfWord(const utl::StringU8& word) const -> int { return word.length() * 2 - 1; }
+
+Word::Word(const utl::StringU8& word)
+    : rawWord(joinCharactersNonBreakable(word))
+    , length(lengthOfWord(word))
+    , styledWord(applyStyle(rawWord)) {}
+
+Word::operator std::string() const { return styledWord; }
+
+void Word::setColor(uint32_t _color) {
+    color = _color;
+    styledWord = applyStyle(rawWord);
+}
+
+void Word::setBackgroundColor(uint32_t _backgroundColor) {
+    backGroundColor = _backgroundColor;
+    styledWord = applyStyle(rawWord);
+}
+
+auto Word::applyStyle(const std::string& str) const -> std::string {
+    constexpr uint32_t colorMask = 0x00FFFFFF;
+
+    std::string style = "";
+    if ((colorMask & color) != 0)
+        style = fmt::format("{}color:#{:06x};", style, (colorMask & color));
+    if ((colorMask & backGroundColor) != 0)
+        style = fmt::format("{}background-color:#{:06x};", style, (colorMask & backGroundColor));
+    if (not style.empty())
+        return fmt::format("<span style=\"{}\">{}</span>", style, str);
+    return str;
+}
+
+}  // namespace markup
--- src/gui/HtmlGen.h
+++ src/gui/HtmlGen.h
@@ -1,13 +1,41 @@
 #pragma once
 #include <utils/StringU8.h>
+#include <string>
 #include <vector>
 
 namespace markup {
-class paragraph {
+
+class Word {
+    const std::string rawWord;
+    auto joinCharactersNonBreakable(const utl::StringU8& word) const -> std::string;
+    auto lengthOfWord(const utl::StringU8& word) const -> int;
+
 public:
-    paragraph() = default;
+    Word(const utl::StringU8& word);
+    operator std::string() const;
+
+    void setColor(uint32_t color);
+    void setBackgroundColor(uint32_t backgroundColor);
+
+    const int length;
+
+private:
+    auto applyStyle(const std::string& str) const -> std::string;
+    uint32_t color = 0;
+    uint32_t backGroundColor = 0;
+    std::string styledWord;
+};
+
+class Paragraph {
+public:
+    Paragraph() = default;
 
 private:
     std::vector<utl::StringU8> words;
 };
 }  // namespace markup
+
+inline std::ostream& operator<<(std::ostream& os, const markup::Word& word) {
+    os << std::string(word);
+    return os;
+}
