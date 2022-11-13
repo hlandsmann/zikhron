ffed39c04aeeef1f3010cf6d794220bb19ee2a0d
Harmen Landsmann <>
2022-11-13 11:50:58 +0100

    update fmt (conan)

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class zikhron(ConanFile):
     requires = (
         "benchmark/1.6.0",
         "catch2/2.13.7",
-        "fmt/8.0.1",
+        "fmt/8.1.1",
         "spdlog/1.9.2",
         "ctre/3.4.1",
         "ms-gsl/3.1.0",
