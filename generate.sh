ce31bc4bf8b922e215b128581651940ec731d0bc
Harmen Landsmann <26793630+hlandsmann@users.noreply.github.com>
2022-11-13 12:17:40 +0100

    generate - don't use temporary build folder anymore...

--- generate.sh
+++ generate.sh
@@ -3,18 +3,8 @@ workspace=$(cd $path_to_srcipt;pwd)
 cd $workspace
 
 build_dir=$workspace/build
-tmp_dir=/run/user/$(id -u)/$(basename $workspace)/build
 
-rm -rf $tmp_dir
-
-if mount | grep $build_dir; then
-    sudo umount $build_dir
-fi
-
-mkdir -p $tmp_dir
 mkdir -p $build_dir
-sudo mount -o bind $tmp_dir $build_dir
-# ln -s $tmp_dir $build_dir
 
 mode="Debug"
 # mode="Release"
