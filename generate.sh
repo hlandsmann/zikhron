d57dd29610ff0499855a2c2eafdbfea38e54594c
Harmen Landsmann <26793630+hlandsmann@users.noreply.github.com>
2023-04-28 22:24:21 +0200

    remove warnings in generate.sh

--- generate.sh
+++ generate.sh
@@ -1,15 +1,15 @@
 path_to_srcipt="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-workspace=$(cd $path_to_srcipt;pwd)
-cd $workspace
+workspace=$(cd "$path_to_srcipt"||exit;pwd)
+cd "$workspace" || exit
 
 build_dir=$workspace/build
 
-mkdir -p $build_dir
+mkdir -p "$build_dir"
 
 mode="Debug"
 # mode="Release"
-cmake -B $build_dir \
+cmake -B "$build_dir" \
     -DCMAKE_BUILD_TYPE=$mode \
     -Dskip_run_conan=OFF \
     --preset=ninja
-(cd $build_dir; cmake --build . -j$(nproc))
+(cd "$build_dir"||exit; cmake --build . -j"$(nproc)")
