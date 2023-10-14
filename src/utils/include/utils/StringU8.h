805bd2fde5290559386e390548c56beebb505214
Harmen Landsmann <26793630+hlandsmann@users.noreply.github.com>
2023-10-14 14:02:37 +0200

    cleanup StringU8

--- src/utils/include/utils/StringU8.h
+++ src/utils/include/utils/StringU8.h
@@ -1,11 +1,13 @@
 #pragma once
-
 #include <fmt/format.h>
-#include <stddef.h>
+
 #include <compare>
+#include <cstddef>
+#include <format>
 #include <iosfwd>
 #include <span>
 #include <string>
+#include <utility>
 #include <vector>
 namespace icu {
 class UnicodeString;
@@ -13,18 +15,28 @@ class UnicodeString;
 
 namespace utl {
 
-template <class T> std::string stringPlusT(std::string&& a, const T& b) { return a += b; }
+template<class T>
+auto stringPlusT(std::string&& a, const T& b) -> std::string
+{
+    return std::move(a) += b;
+}
 
 /* Item representing either a character or a markup formatter */
-class CharU8 {
+class CharU8
+{
 public:
-    CharU8(const std::string& _str, bool _isMarkup, size_t _virtualLength)
-        : str(_str), markup(_isMarkup), virtualLength(_virtualLength){};
-    CharU8(const std::string& _str) : str(_str){};
+    CharU8(std::string _str, bool _isMarkup, size_t _virtualLength)
+        : str{std::move(_str)}, markup{_isMarkup}, virtualLength{_virtualLength} {};
+    CharU8(std::string _str)
+        : str{std::move(_str)} {};
     CharU8(const CharU8&) = default;
+    CharU8(CharU8&&) = default;
+    ~CharU8() = default;
+    auto operator=(const CharU8& other) -> CharU8& = default;
+    auto operator=(CharU8&& other) noexcept -> CharU8& = default;
     operator std::string() const { return str; }
-    auto vLength() const -> size_t { return virtualLength; }
-    auto isMarkup() const -> bool { return markup; }
+    [[nodiscard]] auto vLength() const -> size_t { return virtualLength; }
+    [[nodiscard]] auto isMarkup() const -> bool { return markup; }
     auto operator<=>(const CharU8& other) const -> std::weak_ordering { return str <=> other.str; };
     auto operator==(const CharU8& other) const -> bool = default;
 
@@ -34,52 +46,67 @@ private:
     size_t virtualLength = 1;
 };
 
-class StringU8 {
+class StringU8
+{
     std::vector<CharU8> chars;
 
 public:
     StringU8() = default;
+    ~StringU8() = default;
     StringU8(const std::string&);
     StringU8(const std::span<const CharU8>&);
     // StringU8(const std::string_view&);
     StringU8(const icu::UnicodeString&);
     StringU8(const StringU8&) = default;
     StringU8(StringU8&&) = default;
-    auto operator=(const utl::StringU8& other) -> StringU8&;
+    auto operator=(const StringU8& other) -> StringU8& = default;
+    auto operator=(StringU8&& other) noexcept -> StringU8& = default;
     auto operator<=>(const StringU8&) const -> std::weak_ordering;
     operator std::string() const;
 
-    auto length() const -> size_t;
-    auto vlength() const -> size_t;
-    auto empty() const -> bool;
-    auto at(size_t pos) const -> StringU8;
-    auto substr(size_t pos, size_t n) const -> std::string;
-    auto back() const -> CharU8;
-    auto front() const -> CharU8;
-    auto cbegin() const { return chars.cbegin(); }
-    auto cend() const { return chars.cend(); }
+    [[nodiscard]] auto length() const -> size_t;
+    [[nodiscard]] auto vlength() const -> size_t;
+    [[nodiscard]] auto empty() const -> bool;
+    [[nodiscard]] auto at(size_t pos) const -> StringU8;
+    [[nodiscard]] auto substr(long pos, long n) const -> std::string;
+    [[nodiscard]] auto back() const -> CharU8;
+    [[nodiscard]] auto front() const -> CharU8;
+    [[nodiscard]] auto cbegin() const { return chars.cbegin(); }
+    [[nodiscard]] auto cend() const { return chars.cend(); }
     // void push_back(const std::string&);
     void push_back(const CharU8&);
     void append(const std::string&);
     void append(const icu::UnicodeString&);
 
 private:
-    auto icustringToString(const icu::UnicodeString& str) -> std::string;
+    static auto icustringToString(const icu::UnicodeString& str) -> std::string;
 };
 
-}  // namespace utl
-inline std::ostream& operator<<(std::ostream& os, const utl::CharU8& itemU8) {
+} // namespace utl
+
+inline auto operator<<(std::ostream& os, const utl::CharU8& itemU8) -> std::ostream&
+{
     os << std::string(itemU8);
     return os;
 }
-template <> struct fmt::formatter<utl::CharU8> {
-    template <typename ParseContext> constexpr auto parse(ParseContext& ctx) { return ctx.begin(); }
-    template <typename FormatContext> auto format(const utl::CharU8& itemU8, FormatContext& ctx) {
+
+template<>
+struct std::formatter<utl::CharU8>
+{
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext& ctx)
+    {
+        return ctx.begin();
+    }
+    template<typename FormatContext>
+    auto format(const utl::CharU8& itemU8, FormatContext& ctx)
+    {
         return fmt::format_to(ctx.out(), "{}", std::string(itemU8));
     }
 };
 
-inline std::ostream& operator<<(std::ostream& os, const utl::StringU8& strU8) {
+inline auto operator<<(std::ostream& os, const utl::StringU8& strU8) -> std::ostream&
+{
     os << std::string(strU8);
     return os;
 }
--- src/utils/src/StringU8.cpp
+++ src/utils/src/StringU8.cpp
@@ -1,70 +1,112 @@
 #include "StringU8.h"
+
 #include <unicode/unistr.h>
+#include <unicode/utf.h>
+#include <unicode/utf8.h>
+
 #include <algorithm>
-#include <concepts>
+#include <compare>
+#include <cstddef>
 #include <iterator>
 #include <numeric>
-#include <unicode/utf.h>
+#include <span>
+#include <string>
 
 namespace ranges = std::ranges;
 namespace utl {
 
 // StringU8::StringU8(const std::string_view& _strv) : StringU8(std::string(_strv)) {}
-StringU8::StringU8(const icu::UnicodeString& _str) : StringU8(icustringToString(_str)) {}
-StringU8::StringU8(const std::string& str) { append(str); }
-StringU8::StringU8(const std::span<const CharU8>& items) {
+StringU8::StringU8(const icu::UnicodeString& _str)
+    : StringU8(icustringToString(_str)) {}
+
+StringU8::StringU8(const std::string& str)
+{
+    append(str);
+}
+
+StringU8::StringU8(const std::span<const CharU8>& items)
+{
     ranges::copy(items, std::back_inserter(chars));
 }
 
-StringU8::operator std::string() const { return substr(0, chars.size()); }
-auto StringU8::operator=(const utl::StringU8& other) -> StringU8& {
-    chars = other.chars;
-    return *this;
+StringU8::operator std::string() const
+{
+    return substr(0, static_cast<long>(chars.size()));
 }
 
-auto StringU8::operator<=>(const StringU8& other) const -> std::weak_ordering {
+auto StringU8::operator<=>(const StringU8& other) const -> std::weak_ordering
+{
     return chars <=> other.chars;
 }
 
-auto StringU8::length() const -> size_t { return chars.size(); }
-
-auto StringU8::vlength() const -> size_t {
-    return std::accumulate(
-        chars.cbegin(), chars.cend(), size_t(0), [](const size_t a, const CharU8& b) -> size_t {
-            return a + b.vLength();
-        });
+auto StringU8::length() const -> size_t
+{
+    return chars.size();
 }
-auto StringU8::empty() const -> bool { return chars.empty(); }
-auto StringU8::at(size_t pos) const -> StringU8 {
+
+auto StringU8::vlength() const -> size_t
+{
+    return std::accumulate(
+            chars.cbegin(), chars.cend(), size_t(0), [](const size_t a, const CharU8& b) -> size_t {
+                return a + b.vLength();
+            });
+}
+
+auto StringU8::empty() const -> bool
+{
+    return chars.empty();
+}
+
+auto StringU8::at(size_t pos) const -> StringU8
+{
     StringU8 strU8;
     strU8.push_back(chars.at(pos));
     return strU8;
 }
-auto StringU8::substr(size_t pos, size_t n) const -> std::string {
+
+auto StringU8::substr(long pos, long n) const -> std::string
+{
     auto first = std::min(std::next(chars.begin(), pos), chars.end());
     auto last = std::min(std::next(chars.begin(), pos + n), chars.end());
     return std::accumulate(first, last, std::string{}, stringPlusT<CharU8>);
 }
-auto StringU8::back() const -> CharU8 { return chars.back(); }
-auto StringU8::front() const -> CharU8 { return chars.front(); }
 
-auto StringU8::icustringToString(const icu::UnicodeString& _str) -> std::string {
+auto StringU8::back() const -> CharU8
+{
+    return chars.back();
+}
+
+auto StringU8::front() const -> CharU8
+{
+    return chars.front();
+}
+
+auto StringU8::icustringToString(const icu::UnicodeString& _str) -> std::string
+{
     std::string tempString;
     _str.toUTF8String(tempString);
     return tempString;
 }
 
-void StringU8::push_back(const CharU8& itemU8) { chars.push_back(itemU8); }
-void StringU8::append(const std::string& str) {
-    size_t i = 0, old_i = 0;
+void StringU8::push_back(const CharU8& itemU8)
+{
+    chars.push_back(itemU8);
+}
+
+void StringU8::append(const std::string& str)
+{
+    size_t i = 0;
+    size_t old_i = 0;
     while (i < str.length()) {
         U8_FWD_1(str, i, str.length());
-        chars.push_back(str.substr(old_i, i - old_i));
+        chars.emplace_back(str.substr(old_i, i - old_i));
         old_i = i;
     }
 }
-void StringU8::append(const icu::UnicodeString& uniStr) {
+
+void StringU8::append(const icu::UnicodeString& uniStr)
+{
     const std::string str = icustringToString(uniStr);
     append(str);
 }
-}  // namespace utl
+} // namespace utl
