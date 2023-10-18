e414a7b9f8e8a90b85e88154fdc1f7ec8114363f
Harmen Landsmann <26793630+hlandsmann@users.noreply.github.com>
2023-10-18 07:13:54 +0200

    remove walk function (dead code)

--- src/spaced_repetition/src/TreeWalker.cpp
+++ src/spaced_repetition/src/TreeWalker.cpp
@@ -19,56 +19,11 @@
 #include <ranges>
 #include <set>
 #include <utility>
-#include <vector>
 
 #include <sys/types.h>
 
 namespace ranges = std::ranges;
 namespace views = std::ranges::views;
-
-namespace {
-void walk(const std::shared_ptr<sr::WalkableData>& walkableData)
-{
-    // const auto& cards = walkableData->Cards();
-    // const auto& vocables = walkableData->Vocables();
-    // sr::index_set todayVocablesIndices;
-    //
-    // auto todayVocables = todayVocablesIndices
-    //                      | views::transform([&vocables](size_t index) -> const sr::VocableMeta& {
-    //                            return vocables[index];
-    //                        });
-    // int cardIndex = 0;
-    // for (const auto& card : cards) {
-    //     // if (cardIndex++ == 1200) {
-    //     auto tnv = walkableData->timingAndNVocables(card);
-    //     if (tnv.timing <= 0) {
-    //         todayVocablesIndices.insert(tnv.vocables.begin(), tnv.vocables.end());
-    //     }
-    //     // spdlog::info("Card: {}, when{}, activeVocs:{}", cardIndex++, tnv.timing, tnv.vocables.size());
-    //     // }
-    // }
-    // spdlog::info("Vocables to study: {}", todayVocablesIndices.size());
-    //
-    // auto recency = [&vocables](size_t index) -> float { return vocables[index].Progress().recency(); };
-    //
-    // size_t vocable = *ranges::min_element(todayVocablesIndices, std::less{}, recency);
-    // spdlog::info("voc_index: {}, recency: {}", vocable, vocables[vocable].Progress().recency());
-    // for (size_t cIndex : vocables[vocable].CardIndices()) {
-    //     auto tnv = walkableData->timingAndNVocables(cards[cIndex]);
-    //     spdlog::info("+ cindex: {}, size: {}, time: {}", cIndex, tnv.vocables.size(), tnv.timing);
-    //     for (size_t vIndex : tnv.vocables) {
-    //         spdlog::info("| + vindex: {}, size: {}, recency: {}",
-    //                      vIndex, vocables[vIndex].CardIndices().size(), vocables[vIndex].Progress().recency());
-    //     }
-    //     spdlog::info("...");
-    //     for (size_t vIndex : cards[cIndex].VocableIndices()) {
-    //         spdlog::info("| + vindex: {}, size: {}, recency: {}",
-    //                      vIndex, vocables[vIndex].CardIndices().size(), vocables[vIndex].Progress().recency());
-    //     }
-    // }
-}
-
-} // namespace
 
 namespace sr {
 
