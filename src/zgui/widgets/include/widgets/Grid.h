7f16029557e397d8d1033d660668442a7fe71e9c
Harmen Landsmann <26793630+hlandsmann@users.noreply.github.com>
2024-01-03 22:03:33 +0100

    more grid

--- src/zgui/widgets/include/widgets/Grid.h
+++ src/zgui/widgets/include/widgets/Grid.h
@@ -1,14 +1,69 @@
 #pragma once
 #include "Widget.h"
+
+#include <cstddef>
+#include <memory>
+#include <stdexcept>
+#include <utility>
+#include <vector>
 namespace widget {
 class Grid : public Widget
 {
-    friend class Widget;
-    friend class Box;
-    void setup();
+    using Align = layout::Align;
+    using Orientation = layout::Orientation;
+    using SizeType = layout::SizeType;
 
 public:
+    void setup(std::size_t rows);
     Grid(const WidgetInit& init);
+
+    [[nodiscard]] auto arrange() -> bool override;
+    void setPadding(float padding);
+    void setBorder(float border);
+
+    template<class WidgetType, class... Args>
+    auto add(Align widgetAlign, Args... args) -> std::shared_ptr<WidgetType>
+    {
+        auto widgetRect = std::make_shared<layout::Rect>();
+        WidgetInit init = {.theme = getThemePtr(),
+                           .widgetIdGenerator = getWidgetIdGenerator(),
+                           .rect = widgetRect,
+                           .orientation = Orientation::horizontal,
+                           .align = widgetAlign,
+                           .parent = shared_from_this()};
+        auto widget = std::make_shared<WidgetType>(std::move(init));
+        widget->setup(std::forward<Args>(args)...);
+        widgets.push_back(static_cast<std::shared_ptr<Widget>>(widget));
+        rects.push_back(std::move(widgetRect));
+        resetWidgetSize();
+        setArrangeIsNecessary();
+        return widget;
+    }
+
+    void start();
+    template<class WidgetType>
+    auto next() -> WidgetType&
+    {
+        if (!(currentWidgetIt < widgets.end())) {
+            throw std::out_of_range("Bad call of next(). Forgot to call start()?");
+        }
+        auto& result = dynamic_cast<WidgetType&>(**currentWidgetIt);
+        currentWidgetIt++;
+        return result;
+    }
+
+    constexpr static float s_padding = 16.F;
+private:
+    auto calculateSize() const -> WidgetSize override;
+    auto getRowHeight(std::size_t row) const -> float;
+    auto getColumnWidth(std::size_t column) const -> float;
+    std::size_t rows{};
+    float padding{s_padding};
+    float border{};
+    std::shared_ptr<layout::Rect> borderedRect;
+    std::vector<std::shared_ptr<Widget>> widgets;
+    std::vector<std::shared_ptr<layout::Rect>> rects;
+    std::vector<std::shared_ptr<Widget>>::iterator currentWidgetIt;
 };
 
 } // namespace widget
--- src/zgui/widgets/src/Grid.cpp
+++ src/zgui/widgets/src/Grid.cpp
@@ -1,5 +1,30 @@
 #include <Grid.h>
+#include <memory>
+#include <Widget.h>
 
 namespace widget {
 
+Grid::Grid(const WidgetInit& init)
+    : Widget{init}
+    , borderedRect{std::make_shared<layout::Rect>()}
+{}
+
+auto Grid::arrange() -> bool
+{
+    bool needArrange = false;
+    for (const auto& widget : widgets) {
+        needArrange |= widget->arrange();
+    }
+    return needArrange;
+}
+
+void Grid::setBorder(float _border)
+{
+    border = _border;
+    const auto& rect = Rect();
+    borderedRect->x = rect.x + border;
+    borderedRect->y = rect.y + border;
+    borderedRect->width = rect.width - border * 2;
+    borderedRect->height = rect.height - border * 2;
+}
 }
