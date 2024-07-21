a77a40849c4a3e704bf32cc376fb7a92425a6e59
Harmen Landsmann <26793630+hlandsmann@users.noreply.github.com>
2024-07-21 22:04:11 +0200

    scratch - sig slot test

--- test/scratch.cpp
+++ test/scratch.cpp
@@ -3,7 +3,9 @@
 #include <algorithm>
 #include <cstddef>
 #include <map>
+#include <memory>
 #include <ranges>
+#include <sigslot/signal.hpp>
 #include <string>
 #include <utility>
 #include <vector>
@@ -11,16 +13,41 @@
 namespace ranges = std::ranges;
 namespace views = std::ranges::views;
 
-auto test() -> const std::vector<int>&
+struct ClassA
 {
-    static std::vector<int> res;
-    return res;
-}
+    void doSomething()
+    {
+        spdlog::info("DoSomething");
+        sig_string("DoSomething");
+    }
+
+    sigslot::signal<std::string> sig_string;
+    std::string name = "ClassA";
+};
+
+struct ClassB
+{
+    void rcv_str(const std::string& text)
+    {
+        spdlog::info("rec from {}, text: {}", name, text);
+    }
+
+    std::string name = "ClassB";
+};
 
 auto main() -> int
 {
     spdlog::info("Hello World");
-    auto res = test();
+
+    ClassA classA;
+    classA.doSomething();
+
+    {
+        auto classB = std::make_shared<ClassB>();
+        classA.sig_string.connect(&ClassB::rcv_str, classB);
+        classA.doSomething();
+    }
+    classA.doSomething();
 
     return 0;
 }
