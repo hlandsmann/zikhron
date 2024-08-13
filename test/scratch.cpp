771c7bcbca24759dc71054f015e1bc88923912c2
Harmen Landsmann <26793630+hlandsmann@users.noreply.github.com>
2024-08-13 22:15:29 +0200

    scratch

--- test/scratch.cpp
+++ test/scratch.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <iterator>
 #include <map>
 #include <memory>
 #include <ranges>
@@ -12,6 +13,7 @@
 
 namespace ranges = std::ranges;
 namespace views = std::ranges::views;
+using namespace std::literals;
 
 struct ClassA
 {
@@ -37,17 +39,37 @@ struct ClassB
 
 auto main() -> int
 {
-    spdlog::info("Hello World");
+    // spdlog::info("Hello World");
+    //
+    // ClassA classA;
+    // classA.doSomething();
+    //
+    // {
+    //     auto classB = std::make_shared<ClassB>();
+    //     classA.sig_string.connect(&ClassB::rcv_str, classB);
+    //     classA.doSomething();
+    // }
+    // classA.doSomething();
+    //
+    // for (const char c : "NSAR"s) {
+    //     spdlog::info("{} : {:x}", c, c);
+    // }
 
-    ClassA classA;
-    classA.doSomething();
-
-    {
-        auto classB = std::make_shared<ClassB>();
-        classA.sig_string.connect(&ClassB::rcv_str, classB);
-        classA.doSomething();
+    auto vec1 = std::vector<int>{9, 1, 2, 3, 4, 0, 3, 2, 2, 1};
+    auto vec2 = std::vector<int>{9, 1, 2, 3, 4, 0, 3, 2, 2, 1};
+    auto it1 = std::next(vec1.begin(), 4);
+    spdlog::info("it1: {}", *it1);
+    auto rit1 = std::reverse_iterator{it1};
+    spdlog::info("rit1: {}", *rit1);
+    auto fit = std::find(rit1, std::rend(vec1), 9);
+    spdlog::info("fit1: {}", *fit);
+    auto distance = static_cast<std::size_t>(std::distance(vec1.rbegin(), fit));
+    spdlog::info("index: {}", vec1.size() - distance - 1);
+    auto fit2 = std::next(vec2.rbegin(), distance);
+    spdlog::info("fit2: {}", *fit2);
+    if (fit2 == vec2.rend()) {
+        spdlog::info("is rend");
     }
-    classA.doSomething();
 
     return 0;
 }
