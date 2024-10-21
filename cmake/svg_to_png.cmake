0c4021243cce5cd1fe2c4a73fd3a3cdf3a9b51c5
Harmen Landsmann <26793630+hlandsmann@users.noreply.github.com>
2024-10-21 10:15:36 +0200

    api for imagemagick changed...

--- cmake/svg_to_png.cmake
+++ cmake/svg_to_png.cmake
@@ -5,11 +5,12 @@ function(svg_to_png filename pixel)
   get_filename_component(svg_base_name ${filename} NAME_WLE)
   set(target ${svg_base_name}_${pixel})
   set(png_file ${ICONS_DIRECTORY}/${svg_base_name}_${pixel}px.png)
+  # for some reason ninja needs to be executed with -j1, inkscape won't work correctly ohterwise (dbus error)
   add_custom_command(
       OUTPUT
           ${png_file}
       COMMAND
-          convert +antialias -background none
+          magick +antialias -background none
           ${CMAKE_CURRENT_SOURCE_DIR}/${filename}
           -resize ${pixel}x${pixel} ${png_file}
   )
